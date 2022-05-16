0b849d3f9926b6f5a313fc1510aca170990bf15d
Penna92 <giacomopenna92@gmail.com>
2022-05-16 12:13:33 +0200

    Scaffolding

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <header>
+      <AppHeader />
+      <AppJumbotron />
+    </header>
+    <main>
+      <AppMain />
+    </main>
+    <footer>
+      <AppFooter />
+    </footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+import AppJumbotron from "./components/AppJumbotron.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppJumbotron,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/styles/general.scss";
+</style>
